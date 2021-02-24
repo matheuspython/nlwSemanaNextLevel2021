e86aa53ffc25b08b9fa5bf75df70cf3d52398fd5
matheus souza <ruivorobado@gmail.com>
2021-02-24 10:52:13 -0300

    adicionando o countdown e adicionando o codigo para inicia-lo

--- moveit-next/src/components/Countdown.tsx
+++ moveit-next/src/components/Countdown.tsx
@@ -1,17 +1,41 @@
+import { useState, useEffect} from 'react'
 import styles from '../styles/components/Countdown.module.css'
 
 export function Countdown(){
+  const [time, setTime] = useState(25*60)
+  const [active, setActive] = useState(false)
+
+  const minutes = Math.floor(time/60)
+  const secund = time %60
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
+  const [secondLeft, secondRight] = String(secund).padStart(2, '0').split('')
+
+  function startCountdown(){
+    setActive(true)
+  }
+  useEffect(()=>{
+    if(active && time > 0){
+      setTimeout(()=>{
+        setTime(time - 1)
+      },1000)
+    }
+  },[active, time])
+
   return(
-    <div className={styles.CountdownContainer}>
+    <div>
+      <div className={styles.CountdownContainer}>
       <div>
-        <span>2</span>
-        <span>5</span>
+        <span>{minuteLeft}</span>
+        <span>{minuteRight}</span>
       </div>
       <span>:</span>
       <div>
-        <span>0</span>
-        <span>0</span>
+        <span>{secondLeft}</span>
+        <span>{secondRight}</span>
       </div>
+    </div>
+    <button onClick={startCountdown} type="button" className={styles.countdownButton}>iniciar um novo ciclo</button>
     </div>
   )
 }
